df126e798ae811c7b96fd1f3baea85fc8b44480e
Minhwan Kim <minhwan.kim@streami.co>
2016-11-30 22:56:39 +0900

    add Korean autocompletion functionality

--- endic/endic.go
+++ endic/endic.go
@@ -13,30 +13,38 @@ func main() {
         return
     }
 
-    var url = "http://ac.endic.naver.com/ac?q_enc=utf-8&st=1100&r_format=json&r_enc=utf-8"
-    url = url + "&q=" + os.Args[1]
+	var url = ""
+	for _, url = range []string{
+		"http://ac.endic.naver.com/ac?q_enc=utf-8&st=1100&r_format=json&r_enc=utf-8",
+		"http://ac.endic.naver.com/ac?q_enc=utf-8&st=11001&r_format=json&r_enc=utf-8&r_lt=10001&r_unicode=0&r_escape=1"} {
+		url = url + "&q=" + os.Args[1]
     
-    resp, err := http.Get(url)
-    if err == nil {
-        body, _ := ioutil.ReadAll(resp.Body)
-        resp.Body.Close()
+		resp, err := http.Get(url)
+		if err == nil {
+			body, _ := ioutil.ReadAll(resp.Body)
+			resp.Body.Close()
 
-        type Completion struct {
-            Query []string        `json:"query"`
-            Items [][][][]string  `json:"items"`
-        }
+			type Completion struct {
+				Query []string        `json:"query"`
+				Items [][][][]string  `json:"items"`
+			}
 
-        //fmt.Println(string(body))
-        
-        var c Completion
-        if err := json.Unmarshal(body, &c); err != nil {
-            fmt.Println("Json decoding error!")
-            return
-        }
-        //fmt.Printf("KEYWORD: %s\n", c.Query[0])
-        //fmt.Println("COMPLETION:")
-        for _, elem := range c.Items[0] {
-            fmt.Printf("%s => %s\n", elem[0][0], elem[1][0])
-        }
-    }
+			//fmt.Println(string(body))
+
+			var c Completion
+			if err := json.Unmarshal(body, &c); err != nil {
+				fmt.Println("Json decoding error!")
+				return
+			}
+			//fmt.Printf("KEYWORD: %s\n", c.Query[0])
+			//fmt.Println("COMPLETION:")
+			for _, elem := range c.Items[0] {
+				fmt.Printf("%s => %s\n", elem[0][0], elem[1][0])
+			}
+
+			if len(c.Items[0]) > 0 {
+				return
+			}
+		}
+	}
 }
