c968f8243a4db7ee897458bebb4e571044fa589a
Minhwan Kim <azurelysium@gmail.com>
2016-12-15 14:33:34 +0900

    make keyword urlencoded (in case of space charactor)

--- endic/endic.go
+++ endic/endic.go
@@ -2,6 +2,7 @@ package main
 
 import "os"
 import "fmt"
+import "strings"
 import "net/http"
 import "io/ioutil"
 import "encoding/json"
@@ -17,7 +18,9 @@ func main() {
 	for _, url = range []string{
 		"http://ac.endic.naver.com/ac?q_enc=utf-8&st=1100&r_format=json&r_enc=utf-8",
 		"http://ac.endic.naver.com/ac?q_enc=utf-8&st=11001&r_format=json&r_enc=utf-8&r_lt=10001&r_unicode=0&r_escape=1"} {
-		url = url + "&q=" + os.Args[1]
+
+		keyword := strings.Replace(" ", "%%20", os.Args[1], -1)
+		url = url + "&q=" + keyword
     
 		resp, err := http.Get(url)
 		if err == nil {
